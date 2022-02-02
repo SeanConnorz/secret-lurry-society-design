f872dc9749c04c7b254b3a8f2ccc705dadfa16a8
SeanConnorz <seanandhughes@gmail.com>
2022-02-02 15:23:02 -0500

    Adding chalkboard

--- src/components/Depth.jsx
+++ src/components/Depth.jsx
@@ -1,3 +1,66 @@
+import "../styles/Depth.css";
+
 export default function Depth() {
-  return <section className="h-[100rem]"></section>;
+  window.addEventListener("scroll", () => {
+    const welcome = document.getElementById("welcome");
+    const value = window.scrollY;
+    welcome.style.top = (value - 700) * -1 + "px";
+    if (value < 700) {
+      welcome.style.opacity = (4 / 250) * (value - 600);
+    } else {
+      welcome.style.opacity = 1 - (value - 700) * (1 / 250);
+    }
+    console.log(value);
+  });
+
+  return (
+    <section className="flex flex-col items-center ">
+      <div className="h-[80rem] flex flex-col justify-around w-[80%] text-center text-white">
+        <div id="welcome" className="relative">
+          <h1 className="font-bold text-[2rem]">
+            Welcome to the Secret Lurry Society
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+        <div className="chalkboard">
+          pure css chalkboard
+          <br />
+          <ol>
+            <li>only requires a html tag and div tag</li>
+            <li>
+              thanks to{" "}
+              <a
+                href="http://stackoverflow.com/a/19258938/1654250 "
+                title="Stackoverflow Link"
+                contenteditable="false"
+              >
+                RhinoWalrus
+              </a>{" "}
+              for his help :)
+            </li>
+            <li>
+              thanks to{" "}
+              <a
+                href="https://codepen.io/HugoGiraudel/pen/layxv"
+                title="CodePen Link"
+                contenteditable="false"
+              >
+                HugoGiraudel
+              </a>{" "}
+              for his awesome work :)
+            </li>
+          </ol>
+          <br />
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,41 +1,72 @@
 import "../styles/LandingPage.css";
+import React, { useState, useEffect } from "react";
 
 export default function LandingPage() {
+  const [trash, setTrash] = useState([]);
+
+  const randomNumberBetween = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  const createTrash = (icon, arr) => {
+    const top = randomNumberBetween(0, 50);
+    const size = top / 5 + 1;
+
+    const img = (
+      <img
+        style={{ top: `${top}vh`, left: `${randomNumberBetween(0, 100)}vw` }}
+        className="trash"
+        src={`../../images/trash/${icon}.svg`}
+      ></img>
+    );
+    arr.push(img);
+  };
+
+  useEffect(() => {
+    const trashArr = [];
+    const temp = () => {
+      let counter = 0;
+      while (counter < 5) {
+        createTrash("bag", trashArr);
+        counter++;
+      }
+    };
+    temp();
+    setTrash(trashArr);
+  }, []);
+
   return (
     <section>
-      <div class="landscape">
-        <div class="sun-container sun-container-1"></div>
-        <div class="sun-container">
+      <div className="landscape">
+        <div className="sun-container sun-container-1"></div>
+        <div className="sun-container">
           <img src="../../images/lurry-sun.png" className="sun"></img>
         </div>
-        <div class="sun-container sun-container-reflection">
-          <div class="sun"></div>
+        <div className="sun-container sun-container-reflection">
+          <div className="sun"></div>
         </div>
-        <div class="light"></div>
-        <div class="light light-1"></div>
-        <div class="light light-2"></div>
-        <div class="light light-3"></div>
-        <div class="light light-4"></div>
-        <div class="light light-5"></div>
-        <div class="light light-6"></div>
-        <div class="light light-7"></div>
-        <div class="water"></div>
-        <div class="splash"></div>
-        <div class="splash delay-1"></div>
-        <div class="splash delay-2"></div>
-        <div class="splash splash-4 delay-2"></div>
-        <div class="splash splash-4 delay-3"></div>
-        <div class="splash splash-4 delay-4"></div>
-        <div class="splash splash-stone delay-3"></div>
-        <div class="splash splash-stone splash-4"></div>
-        <div class="splash splash-stone splash-5"></div>
-        <div class="front">
-          <div class="grass"></div>
-          <div class="grass grass-1"></div>
-          <div class="grass grass-2"></div>
-          <div class="reed"></div>
-          <div class="reed reed-1"></div>
+        <div className="flex justify-center absolute w-[20rem] h-[20rem] left-[75vw] bottom-[30vh] z-20 overflow-hidden">
+          <div className="grass-3 relative bottom-0"></div>
+          <div className="reed-3 relative bottom-0"></div>
         </div>
+        <div className="water">
+          {trash}
+          <div className="front">
+            <div className="grass"></div>
+            <div className="reed reed-1"></div>
+            <div className="grass grass-1"></div>
+            <div className="reed"></div>
+          </div>
+        </div>
+        <div className="splash"></div>
+        <div className="splash delay-1"></div>
+        <div className="splash delay-2"></div>
+        <div className="splash splash-4 delay-2"></div>
+        <div className="splash splash-4 delay-3"></div>
+        <div className="splash splash-4 delay-4"></div>
+        <div className="splash splash-stone delay-3"></div>
+        <div className="splash splash-stone splash-4"></div>
+        <div className="splash splash-stone splash-5"></div>
       </div>
     </section>
   );
